2a71570d93d79921d047e2dd35075e5a1eef23a6
Neel <128151091+neeltulsiani007@users.noreply.github.com>
2024-07-04 04:44:32 +0530

    Time: 0 ms (100%), Space: 46.3 MB (27.63%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,42 +1,37 @@
 class Solution {
     public int trap(int[] arr) {
         
-        int lmax[] = new int[arr.length];
-        int rmax[] = new int[arr.length];
-        int cm = arr[0];
-        lmax[0] = arr[0];
-        for(int i =1;i<arr.length;i++)
-        {
-            if(arr[i] > cm)
-            {
-                cm = arr[i];
-                lmax[i] = cm;
-            }
-            else
-            lmax[i] = cm;
-        }
+        int lmax = 0;
+        int rmax = 0;
 
-        cm = arr[arr.length -1];
-
-        for(int i = arr.length-1;i>=0;i--)
-        {
-            if(arr[i] > cm)
-            {
-                cm = arr[i];
-                rmax[i] = cm;
-            }
-            else
-            rmax[i] = cm;
-        }
+        int l =0;
+        int r = arr.length-1;
         int ans = 0;
 
-        for(int i =0;i<arr.length;i++)
+        while(l<=r)
         {
-            ans+=Math.min(lmax[i],rmax[i]) - arr[i];
+            if(arr[l] <= arr[r])
+            {
+                if(arr[l] > lmax)
+                lmax = arr[l];
+                 else
+                {
+                ans+=lmax - arr[l];
+                }
+            l++;
+            }
+            else
+            {
+                if(arr[r] > rmax)
+                rmax = arr[r];
+                else
+                {
+                    ans+=rmax - arr[r];
+                }
+                r--;
+            }   
         }
-
-
         return ans;
-
+     
     }
 }
