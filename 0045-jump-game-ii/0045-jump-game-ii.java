a61d4886c9345820ef672bf77ecbedc232eb3246
Neel <128151091+neeltulsiani007@users.noreply.github.com>
2024-08-09 21:54:51 +0530

    Time: 1 ms (97.83%), Space: 44.7 MB (83.14%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,27 +1,26 @@
 class Solution {
+   
     public int jump(int[] nums) {
         
-        int ans = 0;
+        if(nums.length == 1)
+        return 0;
 
         int cm = 0;
         int ce = 0;
 
-        if(nums.length == 1)
-        return 0;
+        int ans = 0;
 
         for(int i =0;i<nums.length-1;i++)
         {
             if(i+nums[i] > cm)
-            {
-                cm = i+nums[i];
-            }
+            cm = i+nums[i];
+
             if(i == ce)
             {
                 ans++;
-                ce= cm;
+                ce = cm;
             }
         }
-       
         return ans;
     }
 }
