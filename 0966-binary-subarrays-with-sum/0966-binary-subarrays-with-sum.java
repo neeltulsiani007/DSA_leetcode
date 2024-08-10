87c5af61eecc720f18a60471ab2dbe4611aa59ad
Neel <128151091+neeltulsiani007@users.noreply.github.com>
2024-08-11 03:28:13 +0530

    Time: 1 ms (100%), Space: 47.3 MB (72.87%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,30 +1,37 @@
 class Solution {
-    public static int sliding(int[] arr , int goal)
+
+    public int sliding(int[] arr , int goal)
     {
+
         int l =0;
-        int i =0;
+        int r = 0;
         int count = 0;
-        int csum = 0;
-        if(goal<0)
-         return 0;
+        int sum = 0;
+        int n = arr.length;
 
-        while(i<arr.length)
+        if(goal < 0)
+        return 0;
+
+        while(r<n)
         {
-            csum += arr[i];
+            sum += arr[r];
 
-            while(csum > goal)
+            while(sum > goal)
             {
-                csum -= arr[l];
-                l++;
-            } 
+               sum-=arr[l];
+               l++;
+            }
+            
+            count+=r-l+1;
 
-            count+=i-l+1;
-            i++;
+            r++;
+        
         }
+
         return count;
     }
-
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        return sliding(nums , goal) - sliding(nums,goal-1);
+    public int numSubarraysWithSum(int[] arr, int goal) {
+        
+            return sliding(arr ,goal) - sliding(arr , goal-1);
     }
 }
