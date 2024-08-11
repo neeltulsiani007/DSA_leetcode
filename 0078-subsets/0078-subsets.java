a0941894be8cd7123fb875dbafb83f8ea69786a3
Neel <128151091+neeltulsiani007@users.noreply.github.com>
2024-08-12 01:53:39 +0530

    Time: 1 ms (53.01%), Space: 42.7 MB (45.79%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,30 +1,20 @@
 class Solution {
-
-     public static void generate(int[] arr , int n  , List<List<Integer>> l ,ArrayList<Integer> temp , int start)
-    {
-        if(start > n)
-        return;
-        
-        if(start == n)
-        {
-               l.add(new ArrayList<>(temp)); 
-            return;
-        }
-        
-        temp.add(arr[start]);
-        generate(arr , n  , l, temp ,start+1);
-        temp.remove(temp.size()-1);
-        generate(arr , n  , l , temp , start+1);
-        return ;
-        
-        
-    }
-
-    public List<List<Integer>> subsets(int[] arr) {
+    public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> l = new ArrayList<>();
-	    
-	    generate(arr , arr.length , l , new ArrayList<>() , 0);
-	    
-	    return l;
+        
+        int n = 1<<(nums.length);
+
+        for(int i = 0 ;i<n;i++)
+        {
+            List<Integer> t = new ArrayList<>();
+            for(int j =0;j<nums.length;j++)
+            {
+                if((i&(1<<j)) > 0)
+                t.add(nums[j]);
+            }
+            l.add(t);
+        }
+        return l;
+
     }
 }
