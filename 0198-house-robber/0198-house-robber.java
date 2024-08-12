21292ca64faf7e51dd944b80b96a72f33cd1917e
Neel <128151091+neeltulsiani007@users.noreply.github.com>
2024-08-12 20:58:59 +0530

    Time: 0 ms (100%), Space: 40.7 MB (85.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,23 @@
 class Solution {
 
-    public int rob(int[] nums) {
-       
-        int n = nums.length;
-        if(n == 1)
-        return nums[0];
-        if(n==2)
-        return Math.max(nums[0] , nums[1]);
+    public int ans(int[] nums , int ind , int[] memo, int n)
+    {
+        if(ind > n-1)
+        return 0;
 
-        int[] f = new int[n+1];
-        f[1] = nums[0];
-        f[2] = Math.max(nums[0] , nums[1]);
+        if(memo[ind] != -1)
+        return memo[ind];
 
-        for(int i = 3;i<=n;i++)
-        {
-            f[i] = Math.max(f[i-1] , f[i-2] + nums[i-1]);
-        }
-
-        return f[n];
+        int curr = Integer.MIN_VALUE;
+        int curr2 = Integer.MIN_VALUE;
+         curr = 0 + ans(nums,ind+1 , memo,n);
+         curr2 = nums[ind] + ans(nums , ind+2 , memo,n);
+        return memo[ind] = Math.max(curr , curr2);
     }
-    
+    public int rob(int[] nums) {
+        int[] memo = new int[nums.length+1];
+        Arrays.fill(memo , -1);
+        int n = nums.length;
+        return ans(nums , 0 , memo , n);
+    }
 }
