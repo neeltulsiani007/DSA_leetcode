f9fad021e4724068f418099e19c8f16edff389eb
Neel <128151091+neeltulsiani007@users.noreply.github.com>
2024-08-12 21:16:45 +0530

    Time: 0 ms (100%), Space: 40.9 MB (46.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,42 +1,30 @@
 class Solution {
-    public int fun(int[] nums) {
-      
-        int n = nums.length;
-        if(n == 1)
-        return nums[0];
-        if(n==2)
-        return Math.max(nums[0] , nums[1]);
+    public int ans(int[] nums , int ind , int[] memo, int n)
+    {
+        if(ind > n-1)
+        return 0;
 
-        int[] f = new int[n+1];
-        f[1] = nums[0];
-        f[2] = Math.max(nums[0] , nums[1]);
+        if(memo[ind] != -1)
+        return memo[ind];
 
-        for(int i = 3;i<=n;i++)
-        {
-            f[i] = Math.max(f[i-1] , f[i-2] + nums[i-1]);
-        }
-
-        return f[n];
+        int curr = Integer.MIN_VALUE;
+        int curr2 = Integer.MIN_VALUE;
+         curr = 0 + ans(nums,ind+1 , memo,n);
+         curr2 = nums[ind] + ans(nums , ind+2 , memo,n);
+        return memo[ind] = Math.max(curr , curr2);
     }
-
     public int rob(int[] nums) {
 
-            int n = nums.length;
-         if(n == 1)
-        return nums[0];
-        if(n==2)
-        return Math.max(nums[0] , nums[1]);
-        int n1[] = new int[nums.length-1];
-        int n2[] = new int[nums.length-1];
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo , -1);
+        int n = nums.length;
 
-        for(int i =0;i<nums.length-1;i++)
-        {
-            n1[i] = nums[i];
-        }
-        for(int i =1;i<nums.length;i++)
-        {
-            n2[i-1] = nums[i];
-        }
-        return Math.max(fun(n1) , fun(n2));
+        if(n==1)
+        return nums[0];
+        if(n == 2)
+        return Math.max(nums[0],nums[1]);
+        int val1 = ans(nums , 0 , memo , n-1);
+        Arrays.fill(memo , -1);
+        return Math.max(val1 ,ans(nums , 1 , memo , n));
     }
 }
