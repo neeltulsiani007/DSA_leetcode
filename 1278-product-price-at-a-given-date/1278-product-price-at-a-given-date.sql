6815ef240430622d482b664e4cde5c15a77ba726
Neel <128151091+neeltulsiani007@users.noreply.github.com>
2024-09-11 20:12:28 +0530

    Time: 382 ms (98.7%), Space: 0B (100%) - LeetHub

---
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -0,0 +1,3 @@
+select p1.product_id , p1.new_price as price from Products p1 where (p1.product_id,p1.change_date) in (select product_id , max(change_date) as date from Products where change_date <='2019-08-16' group by product_id)
+union
+select distinct product_id , 10 as price from Products where product_id not in (select product_id from Products where change_date <='2019-08-16' );
